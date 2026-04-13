f75c6292649a88eee82efac5184e4580bc12c1c0
Jan Myziak <s31003@pjwstk.edu.pl>
2026-04-14 01:50:54 +0200

    Added room and reservation models

---
+++ Models/Reservations.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PJATK_APBD_Cw5_s31003.Models;
+
+public class Reservation : IValidatableObject
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+
+    [Required(ErrorMessage = "Imie organizatora jest wymagane.")]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Temat jest wymagany.")]
+    public string Topic { get; set; } = string.Empty;
+
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public string Status { get; set; } = "planned";
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "Czas zakonczenia (EndTime) musi być pozniejszy niż czas rozpoczecia (StartTime).",
+                new[] { nameof(EndTime), nameof(StartTime) });
+        }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PJATK_APBD_Cw5_s31003.Models;
+
+public class Room
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Nazwa sali jest wymagana.")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Kod budynku jest wymagany.")]
+    public string BuildingCode { get; set; } = string.Empty;
+
+    public int Floor { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "Pojemnosc musi byc wieksza od zera.")]
+    public int Capacity { get; set; }
+
+    public bool HasProjector { get; set; }
+    
+    public bool IsActive { get; set; }
+}
